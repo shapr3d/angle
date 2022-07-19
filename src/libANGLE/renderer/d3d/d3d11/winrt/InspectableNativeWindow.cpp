50ff4a7c6d10cfd3855ff3985a73705dde4d132e
Koncz Levente <palver123@users.noreply.github.com>
2022-07-19 18:29:38 +0200

    Revert "[GFX-591] Can pass external swapchain to D3D11 as EGL native window (#8)" (#12)
    This reverts commit 4d7a045be12d0735a4d4b819b00ba84be9308806.

--- src/libANGLE/renderer/d3d/d3d11/winrt/InspectableNativeWindow.cpp
+++ src/libANGLE/renderer/d3d/d3d11/winrt/InspectableNativeWindow.cpp
@@ -57,26 +57,6 @@ bool IsSwapChainPanel(EGLNativeWindowType window,
     return false;
 }
 
-bool IsSwapChain(EGLNativeWindowType window, ComPtr<IDXGISwapChain>* swapChain) {
-    if (!window)
-    {
-        return false;
-    }
-
-    ComPtr<IInspectable> win = window;
-    ComPtr<IDXGISwapChain> sc;
-    if (SUCCEEDED(win.As(&sc)))
-    {
-        if (swapChain != nullptr)
-        {
-            *swapChain = std::move(sc);
-        }
-        return true;
-    }
-
-    return false;
-}
-
 bool IsEGLConfiguredPropertySet(EGLNativeWindowType window,
                                 ABI::Windows::Foundation::Collections::IPropertySet **propertySet,
                                 IInspectable **eglNativeWindow)
