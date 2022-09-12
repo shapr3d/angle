ec8335f195b15b887764dcb2f60bb2e6ad9678e3
Peter Jámbor <peter.jambor@shapr3d.com>
2022-09-12 09:31:39 +0200

    Revert "[GFX-2005] External buffer storage is StaticDraw by default (D3D11) (#18)"
    This reverts commit 0033988d8555a4dfdf99aa60b6805b3357cf2356.

--- src/libANGLE/renderer/d3d/d3d11/Buffer11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Buffer11.cpp
@@ -402,9 +402,7 @@ angle::Result Buffer11::setDataWithUsageFlags(const gl::Context *context,
         // calling BufferStorage() (and as consequence BufferStorageExternal())
         // modifies the buffer object state such that BUFFER_USAGE is set to
         // DYNAMIC_DRAW.
-        // Going against the spec, we choose STATIC_DRAW as a workaround aiming 
-        // to avoid staging and system memory copies.
-        updateD3DBufferUsage(context, gl::BufferUsage::StaticDraw);
+        updateD3DBufferUsage(context, gl::BufferUsage::DynamicDraw);
 
         auto *contextD3D     = GetImplAs<ContextD3D>(context);
         d3d11::Buffer buffer(d3d11::DynamicCastComObject<ID3D11Buffer>(static_cast<IUnknown *>(clientBuffer)), nullptr);
